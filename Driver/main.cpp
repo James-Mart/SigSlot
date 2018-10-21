fce9f8cf518d844986366dc05371586f911b3f41
James Mart <james@centsability.io>
2018-10-21 19:23:50 -0400

    Refactor timer

--- Driver/main.cpp
+++ Driver/main.cpp
@@ -3,18 +3,30 @@
 #include <chrono>
 #include "SigSlot/SigSlot.h"
 
+struct Timer
+{
+    Timer()
+    {
+        start = std::chrono::high_resolution_clock::now();
+    }
+    ~Timer()
+    {
+        duration = (std::chrono::high_resolution_clock::now() - start);
+        std::cout << "Function timer took " << duration.count() * 1000.0f << "ms\n";
+    }
+
+    std::chrono::time_point<std::chrono::steady_clock> start;
+    std::chrono::duration<float> duration;
+};
+
 int main()
 {
-    auto start = std::chrono::high_resolution_clock::now();
+    Timer timer;
 
     std::function<void(int, int)> func = [](int p1, int p2) {std::cout << p1 << " " << p2 << '\n'; };
     std::vector<std::function<void(int, int)>> funcs = {func};
  
     SlotExecution::ExecuteSlots(funcs, 1, 2);
-
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<float> duration = end - start;
-    std::cout << "Function took " << duration.count() * 1000 << "ms\n";
 
     return 0;
 }
