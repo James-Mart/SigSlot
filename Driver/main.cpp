7751b99694916daa116e57724a4f1fd27b9a96c9
James Mart <james@centsability.io>
2018-10-21 19:57:05 -0400

    Add failable slot functions

--- Driver/main.cpp
+++ Driver/main.cpp
@@ -5,13 +5,23 @@
 
 int main()
 {
-    SigSlotUtils::Timer timer([](float p_time) {std::cout << "Function timer took " << p_time << "ms\n"; });
+    SigSlotUtils::Timer timer([](float p_time) {std::cout << "Function took " << p_time << "ms\n"; });
 
+    // Run regular slot functions
     std::function<void(int, int)> funcPrint = [](int p1, int p2) {std::cout << "Arguments: " << p1 << " " << p2 << '\n'; };
     std::function<void(int, int)> funcSum = [](int p1, int p2) {std::cout << "Sum: " << p1 + p2 << '\n'; };
     std::vector<std::function<void(int, int)>> funcs = {funcPrint, funcSum};
- 
     SlotExecution::ExecuteSlots(funcs, 1, 2);
+
+    // Run failable slot functions
+    std::function<bool(int)> funcPrintInt    = [](int p1) {std::cout << "Int: " << p1 << '\n'; return true; };
+    std::function<bool(int)> funcReturnFalse = [](int p1) {return false; };
+    std::vector<std::function<bool(int)>> failableFuncs = { funcPrintInt, funcReturnFalse };
+    int iNrSucceeded = SlotExecution::ExecuteSlotsUntilFail(failableFuncs, 3);
+    if (iNrSucceeded < static_cast<int>(failableFuncs.size()))
+        std::cout << "Only " << iNrSucceeded << " out of " << failableFuncs.size() << " failable functions succeeded.\n";
+
+
 
     return 0;
 }
--- SigSlot.h
+++ SigSlot.h
@@ -6,11 +6,23 @@
 namespace SlotExecution
 {
     template<typename ...Ts>
-    void ExecuteSlots(const std::vector<std::function<void(Ts...)>> p_funcs, Ts... p_args)
+    void ExecuteSlots(const std::vector<std::function<void(Ts...)>>& p_slots, Ts... p_args)
     {
-        for (const auto& func : p_funcs)
+        for (const auto& func : p_slots)
         {
             func(p_args...);
         }
+    }
+
+    template<typename ...Ts>
+    int ExecuteSlotsUntilFail(const std::vector<std::function<bool(Ts...)>>& p_slots, Ts... p_args)
+    {
+        int iLastNr = 0;
+        for (const auto& func : p_slots)
+        {
+            if (!func(p_args...)) break;
+            ++iLastNr;
+        }
+        return iLastNr;
     }
 };
