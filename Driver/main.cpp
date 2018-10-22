bafe5fa1381bd2483b0e0b1f50ccd8ea7646537f
James Mart <james@centsability.io>
2018-10-22 00:02:34 -0400

    Added intial signal emitter capability

--- Driver/main.cpp
+++ Driver/main.cpp
@@ -3,6 +3,42 @@
 #include "SigSlot/SigSlot.h"
 #include "SigSlot/Driver/Utilities/Timer.h"
 
+class SomeObject : public SigSlot::Actionables::CanEmit
+{
+public:
+    SomeObject() : m_number(0) {}
+    virtual ~SomeObject() {}
+    void Modify(int p_newNumber) { 
+        m_number = p_newNumber;
+        EmitSignal("NumberChanged");
+    }
+
+private:
+    int m_number;
+};
+
+class SomeProcess
+{
+public:
+    void AddObject(const SomeObject& obj)
+    {
+        m_objects.push_back(obj);
+    }
+    void Run()
+    {
+        for (auto& object : m_objects)
+        {
+            for (int i = 0; i < 1000; ++i)
+            {
+                object.Modify(i);
+            }
+        }
+    }
+
+private:
+    std::vector<SomeObject> m_objects;
+};
+
 int main()
 {
     SigSlotUtils::Timer timer([](float p_time) {std::cout << "Function took " << p_time << "ms\n"; });
@@ -11,16 +47,24 @@ int main()
     std::function<void(int, int)> funcPrint = [](int p1, int p2) {std::cout << "Arguments: " << p1 << " " << p2 << '\n'; };
     std::function<void(int, int)> funcSum = [](int p1, int p2) {std::cout << "Sum: " << p1 + p2 << '\n'; };
     std::vector<std::function<void(int, int)>> funcs = {funcPrint, funcSum};
-    SlotExecution::ExecuteSlots(funcs, 1, 2);
+    SigSlot::Executor::Execute(funcs, 1, 2);
 
     // Run failable slot functions
     std::function<bool(int)> funcPrintInt    = [](int p1) {std::cout << "Int: " << p1 << '\n'; return true; };
     std::function<bool(int)> funcReturnFalse = [](int p1) {return false; };
     std::vector<std::function<bool(int)>> failableFuncs = { funcPrintInt, funcReturnFalse };
-    int iNrSucceeded = SlotExecution::ExecuteSlotsUntilFail(failableFuncs, 3);
+    int iNrSucceeded = SigSlot::Executor::ExecuteUntilFail(failableFuncs, 3);
     if (iNrSucceeded < static_cast<int>(failableFuncs.size()))
         std::cout << "Only " << iNrSucceeded << " out of " << failableFuncs.size() << " failable functions succeeded.\n";
 
+
+    SomeProcess process;
+    SomeObject obj;
+    /*obj.OnSignal_CheckCriteria("NumberChanged", 
+        [](int p_iNewNumber) -> bool 
+    {
+        if (p_iNewNumber % 2 == 0) return true;
+    });*/
 
 
     return 0;
--- SigSlot.h
+++ SigSlot.h
@@ -2,27 +2,69 @@
 
 #include <functional>
 #include <vector>
+#include <unordered_map>
 
-namespace SlotExecution
+namespace SigSlot
 {
-    template<typename ...Ts>
-    void ExecuteSlots(const std::vector<std::function<void(Ts...)>>& p_slots, Ts... p_args)
+    namespace Executor
     {
-        for (const auto& func : p_slots)
+        template<typename ...Ts>
+        void Execute(const std::vector<std::function<void(Ts...)>>& p_slots, Ts... p_args)
         {
-            func(p_args...);
+            for (const auto& func : p_slots)
+            {
+                func(p_args...);
+            }
         }
-    }
 
-    template<typename ...Ts>
-    int ExecuteSlotsUntilFail(const std::vector<std::function<bool(Ts...)>>& p_slots, Ts... p_args)
-    {
-        int iLastNr = 0;
-        for (const auto& func : p_slots)
+        template<typename ...Ts>
+        int ExecuteUntilFail(const std::vector<std::function<bool(Ts...)>>& p_slots, Ts... p_args)
         {
-            if (!func(p_args...)) break;
-            ++iLastNr;
+            int iCount = 0;
+            for (const auto& func : p_slots)
+            {
+                if (!func(p_args...)) break;
+                ++iCount;
+            }
+            return iCount;
         }
-        return iLastNr;
-    }
+    };
+
+    namespace Actionables
+    {
+        class CanEmit
+        {
+        public:
+            virtual ~CanEmit() { /* NOP */ }
+            void EmitSignal(std::string p_signalId)
+            {
+                auto slotsIter = m_vecFailableSlots.find(p_signalId);
+                if (slotsIter != m_vecFailableSlots.end()) 
+                {
+                    const auto& slots = slotsIter->second;
+                    SignalEmitted(p_signalId, (slots.size() != Executor::ExecuteUntilFail(slots)));
+                }
+            }
+
+            template <typename T, typename... Us>
+            void OnSignal_CheckCriteria(std::string p_signalId, const std::function<T(Us...)>& p_criteriaFunction, const T& p_expectedResult, Us... p_args)
+            {
+                m_vecFailableSlots[p_signalId].push_back([&]() -> bool
+                {
+                    return (p_criteriaFunction(p_args...) == p_expectedResult);
+                });
+            }
+
+
+        protected:
+            virtual void SignalEmitted(std::string p_signalId, bool p_criteriaPassed)
+            {
+                // NOP - Override in child class to respond to passed/failed criteria checks
+            }
+
+        private:
+            std::unordered_map<std::string, std::vector<std::function<bool()>>> m_vecFailableSlots;
+            
+        };
+    };
 };
