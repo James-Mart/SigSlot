21413c3d61b1834be69fdede1b87456011c3659f
James Mart <james@centsability.io>
2018-10-22 21:01:32 -0400

    Add test, alert, and filter modes of signalling.

--- Driver/main.cpp
+++ Driver/main.cpp
@@ -3,17 +3,41 @@
 #include "SigSlot/SigSlot.h"
 #include "SigSlot/Driver/Utilities/Timer.h"
 
-class SomeObject : public SigSlot::Actionables::CanEmit
+class SomeObject : public SigSlot::CanAlert
+                 , public SigSlot::CanFilter
+                 , public SigSlot::CanTest
 {
 public:
-    SomeObject() : m_number(0) {}
+    SomeObject() : m_number(0) 
+    {
+        std::function<bool()> validationFunction = std::bind(&SomeObject::_IsValidData, this);
+        OnSignal_Test("DataValid", validationFunction, true);
+    }
     virtual ~SomeObject() {}
-    void Modify(int p_newNumber) { 
-        m_number = p_newNumber;
-        EmitSignal("NumberChanged");
+
+    /*void Modify(int p_newNumber) { 
+        EmitSignal_Filter("FilterNumberChanging", p_newNumber);
+        if (m_number != p_newNumber)
+        {
+            m_number = p_newNumber;
+            EmitSignal_Alert("AlertNumberChanged");
+        }
+    }*/
+
+    void Modify2(int p_newNumber)
+    {
+        bool bValid = EmitSignal_Test("DataValid");
+        if (bValid)
+        {
+            m_number = p_newNumber;
+            EmitSignal_Alert("AlertNumberChanged");
+        }
     }
 
 private:
+    inline bool _IsValidData() const {
+        return true;
+    }
     int m_number;
 };
 
@@ -28,9 +52,9 @@ public:
     {
         for (auto& object : m_objects)
         {
-            for (int i = 0; i < 1000; ++i)
+            for (int i = 0; i < 100; ++i)
             {
-                object.Modify(i);
+                object.Modify2(i);
             }
         }
     }
@@ -39,32 +63,33 @@ private:
     std::vector<SomeObject> m_objects;
 };
 
-int main()
+void TestSlotFunctions() 
 {
-    SigSlotUtils::Timer timer([](float p_time) {std::cout << "Function took " << p_time << "ms\n"; });
-
     // Run regular slot functions
     std::function<void(int, int)> funcPrint = [](int p1, int p2) {std::cout << "Arguments: " << p1 << " " << p2 << '\n'; };
     std::function<void(int, int)> funcSum = [](int p1, int p2) {std::cout << "Sum: " << p1 + p2 << '\n'; };
-    std::vector<std::function<void(int, int)>> funcs = {funcPrint, funcSum};
+    std::vector<std::function<void(int, int)>> funcs = { funcPrint, funcSum };
     SigSlot::Executor::Execute(funcs, 1, 2);
 
     // Run failable slot functions
-    std::function<bool(int)> funcPrintInt    = [](int p1) {std::cout << "Int: " << p1 << '\n'; return true; };
+    std::function<bool(int)> funcPrintInt = [](int p1) {std::cout << "Int: " << p1 << '\n'; return true; };
     std::function<bool(int)> funcReturnFalse = [](int p1) {return false; };
     std::vector<std::function<bool(int)>> failableFuncs = { funcPrintInt, funcReturnFalse };
     int iNrSucceeded = SigSlot::Executor::ExecuteUntilFail(failableFuncs, 3);
     if (iNrSucceeded < static_cast<int>(failableFuncs.size()))
         std::cout << "Only " << iNrSucceeded << " out of " << failableFuncs.size() << " failable functions succeeded.\n";
+}
 
+int main()
+{
+    SigSlotUtils::Timer timer([](float p_time) {std::cout << "Function took " << p_time << "ms\n"; });
+
+    //TestSlotFunctions();
 
     SomeProcess process;
-    SomeObject obj;
-    /*obj.OnSignal_CheckCriteria("NumberChanged", 
-        [](int p_iNewNumber) -> bool 
-    {
-        if (p_iNewNumber % 2 == 0) return true;
-    });*/
+    SomeObject obj;    
+    process.AddObject(obj);
+    process.Run();
 
 
     return 0;
--- SigSlot.h
+++ SigSlot.h
@@ -30,41 +30,94 @@ namespace SigSlot
         }
     };
 
-    namespace Actionables
+    class CanAlert
     {
-        class CanEmit
+    public:
+        virtual ~CanAlert() { /* NOP */ }
+
+        void EmitSignal_Alert(const std::string& p_signalId)
         {
-        public:
-            virtual ~CanEmit() { /* NOP */ }
-            void EmitSignal(std::string p_signalId)
+            auto slotsIter = m_vecSlots_Alerts.find(p_signalId);
+            if (slotsIter != m_vecSlots_Alerts.end())
             {
-                auto slotsIter = m_vecFailableSlots.find(p_signalId);
-                if (slotsIter != m_vecFailableSlots.end()) 
-                {
-                    const auto& slots = slotsIter->second;
-                    SignalEmitted(p_signalId, (slots.size() != Executor::ExecuteUntilFail(slots)));
-                }
+                Executor::Execute(slotsIter->second);
             }
+        }
 
-            template <typename T, typename... Us>
-            void OnSignal_CheckCriteria(std::string p_signalId, const std::function<T(Us...)>& p_criteriaFunction, const T& p_expectedResult, Us... p_args)
+        template <typename... Ts>
+        void OnSignal_Alert(const std::string& p_signalId, const std::function<void(Ts...)>& p_alertFunction, Ts... p_args)
+        {
+            m_vecSlots_Alerts[p_signalId].push_back([&]() {
+                p_alertFunction(p_args...);
+            });
+        }
+
+    private:
+        std::unordered_map<std::string, std::vector<std::function<void()>>> m_vecSlots_Alerts;
+    };
+
+    class CanTest
+    {
+    public:
+        virtual ~CanTest() { /* NOP */ }
+
+        bool EmitSignal_Test(const std::string& p_signalId)
+        {
+            auto slotsIter = m_vecSlots_Tests.find(p_signalId);
+            if (slotsIter != m_vecSlots_Tests.end())
             {
-                m_vecFailableSlots[p_signalId].push_back([&]() -> bool
-                {
-                    return (p_criteriaFunction(p_args...) == p_expectedResult);
-                });
+                const auto& slots = slotsIter->second;
+                return (slots.size() != Executor::ExecuteUntilFail(slots));
             }
+        }
 
-
-        protected:
-            virtual void SignalEmitted(std::string p_signalId, bool p_criteriaPassed)
-            {
-                // NOP - Override in child class to respond to passed/failed criteria checks
-            }
-
-        private:
-            std::unordered_map<std::string, std::vector<std::function<bool()>>> m_vecFailableSlots;
+        template <typename T, typename... Us>
+        void OnSignal_Test(const std::string& p_signalId, const std::function<T(Us...)>& p_criteriaFunction, const T& p_expectedResult, Us... p_args)
+        {
+            m_vecSlots_Tests[p_signalId].push_back([&]() -> bool {
+                return (p_criteriaFunction(p_args...) == p_expectedResult);
+            });
             
-        };
+        }
+
+    private:
+        std::unordered_map<std::string, std::vector<std::function<bool()>>> m_vecSlots_Tests;
+    };
+
+    class CanFilter
+    {
+        /* Open design questions:
+        *   1) How should we prioritize filters? Run in the order they are added? Provide a prioritization system?
+        *      For now, I will just run in registration order.
+        *   2) When a value is filtered, do we stop processing the remaining filters? Do we pass the filtered value
+        *      to the remaining filters?
+        *      For now, I will stop once a single filter changes the 
+        */
+
+    public:
+        virtual ~CanFilter() { /* NOP */ }
+
+        template <typename T>
+        void EmitSignal_Filter(const std::string& p_signalId)
+        {
+            auto slotsIter = m_vecSlots_Filters.find(p_signalId);
+            if (slotsIter != m_vecSlots_Filters.end())
+            {
+                const auto& slots = slotsIter->second;
+                return (slots.size() != Executor::ExecuteUntilFail(slots));
+            }
+        }
+
+        template <typename T, typename... Us>
+        void OnSignal_Filter(const std::string& p_signalId, const std::function<T(Us...)>& p_filterFunction, Us... p_args)
+        {
+            m_vecSlots_Filters[p_signalId].push_back([&]() -> T {
+                return p_filterFunction(p_args...);
+            });
+        }
+
+    private:
+        //Todo: Make this a variadic class template to allow many filterable types
+        std::unordered_map<std::string, std::vector<std::function<int()>>> m_vecSlots_Filters; 
     };
 };
