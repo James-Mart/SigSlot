b8cd092173c6a2c75c86dfcb2892d403170e27c0
James Mart <james@centsability.io>
2018-12-04 13:34:16 -0500

    Clean up SigSlot interface, add new signal type

--- Driver/main.cpp
+++ Driver/main.cpp
@@ -1,15 +1,37 @@
 #include <functional>
 #include <iostream>
+#include <initializer_list>
+#include <memory>
+
 #include "SigSlot/SigSlot.h"
 #include "SigSlot/Driver/Utilities/Timer.h"
 
-//Objects must specify what signal behavior they require
-class SomeObject : public SigSlot::SignalValueChanged<int>
-                 , public SigSlot::SignalFilterValue<int&>
+enum class ObjectAEvents {
+    Event_beforeExecute,
+    Event_afterExecute
+};
+
+class ObjectA : public SigSlot::SignalAlertEvent<ObjectAEvents>
 {
 public:
-    SomeObject() : m_number(-1) { /*NOP*/ }
-    virtual ~SomeObject() {}
+    ObjectA() {}
+    virtual ~ObjectA() {}
+
+    void Execute()
+    {
+        EmitSignal_AlertEvent(ObjectAEvents::Event_beforeExecute);
+        std::cout << "Object A execution!\n";
+        EmitSignal_AlertEvent(ObjectAEvents::Event_afterExecute);
+    }
+};
+
+//Objects must specify what signal behavior they require
+class ObjectB : public SigSlot::SignalValueChanged<int>
+              , public SigSlot::SignalFilterValue<int&>
+{
+public:
+    ObjectB() : m_number(-1) { /*NOP*/ }
+    virtual ~ObjectB() {}
 
     void Modify(int p_newNumber) {
         //Objects that use this pattern should emit signals where appropriate
@@ -28,39 +50,51 @@ private:
 class SomeProcess
 {
 public:
-    void AddObject(const SomeObject& obj) {
-        m_objects.push_back(obj);
+    SomeProcess(std::initializer_list<std::shared_ptr<ObjectB>> objects)
+    {
+        for (auto o : objects) m_objects.emplace_back(o);
     }
 
     void Run() {
         // Other processes may use objects as they normally would
-        for (auto& object : m_objects) {
+        for (auto o : m_objects) {
             for (int i = 0; i < 11; ++i) {
-                object.Modify(i);
+                o->Modify(i);
             }
         }
     }
 
 private:
-    std::vector<SomeObject> m_objects;
+    std::vector<std::shared_ptr<ObjectB>> m_objects;
 };
 
 int main() {
     JmUtils::Timer timer([](float p_time) {std::cout << "Function took " << p_time << "ms\n"; });
 
-    SomeObject obj1;
-    obj1.OnSignal_ValueChanged([](const int& i) {std::cout << "Object 1 data: " << i << '\n'; });
-    obj1.OnSignal_FilterValue([](int& i) { i *= 2; }); //  0 -> 20
-    obj1.OnSignal_FilterValue([](int& i) { i -= 1; }); // -1 -> 19
+    std::cout << "------- BEGIN: event emission test --------\n";
+    ObjectA eventEmitter;
+    eventEmitter.Execute();
+    std::cout << "Attaching pre/post events...\n";
+    eventEmitter.OnSignal_AlertEvent(ObjectAEvents::Event_beforeExecute, (std::function<void()>)[]() {std::cout << "SLOT: Before execution!\n"; });
+    eventEmitter.OnSignal_AlertEvent(ObjectAEvents::Event_afterExecute, (std::function<void()>)[]() {std::cout << "SLOT: After execution!\n"; });
+    eventEmitter.Execute();
+    std::cout << "------- END event emission test --------\n\n";
 
-    SomeObject obj2;
-    obj2.OnSignal_ValueChanged([](const int& i) {std::cout << "Object 2 data: " << i << '\n'; });
-    obj2.OnSignal_FilterValue([](int& i) { i = 0; }); //  Always 0
-
-    SomeProcess process;
-    process.AddObject(obj1);
-    process.AddObject(obj2);
+    std::cout << "------- BEGIN value changed test --------\n";
+    auto obj1 = std::make_shared<ObjectB>();
+    auto obj2 = std::make_shared<ObjectB>();
+    SomeProcess process{ obj1, obj2 };
+    obj1->OnSignal_ValueChanged([](const int& i) {std::cout << "Object 1 data: " << i << '\n'; });
+    obj2->OnSignal_ValueChanged([](const int& i) {std::cout << "Object 2 data: " << i << '\n'; });
     process.Run();
+    std::cout << "------- END value changed test --------\n\n";
+
+    std::cout << "------- BEGIN value filtered test --------\n";
+    obj1->OnSignal_FilterValue([](int& i) { i *= 2; }); //  0 -> 20
+    obj1->OnSignal_FilterValue([](int& i) { i -= 1; }); // -1 -> 19
+    obj2->OnSignal_FilterValue([](int& i) { i = 0; }); //  Always 0
+    process.Run();
+    std::cout << "------- END value filtered test --------\n";
 
     return 0;
 }
--- SigSlot.h
+++ SigSlot.h
@@ -9,56 +9,83 @@ namespace SigSlot
     /*****************************************************************
     * SignalType provides the basic signal interface
     ******************************************************************/
-    template <typename T, typename... Us>
+    template <typename... Us>
     class SignalType
     {
     public:
         virtual ~SignalType() { /* NOP */ }
 
-    protected: 
-
-        void OnSignal(const std::function<T(Us...)>& p_alertFunction) {
+        void OnSignal(const std::function<void(Us...)>& p_alertFunction) {
             m_vecSlots.push_back(p_alertFunction);
         }
 
+        template <typename T>
+        void OnSpecificSignal(T p_slotID, const std::function<void()>& p_slotFunction)
+        {
+            m_vecSlots.emplace_back([=](T p_ID) {
+                if (p_slotID == p_ID) p_slotFunction();
+            });
+        }
+
+    protected: 
         void Emit(Us... p_args) {
             for (const auto& func : m_vecSlots) {
                 func(p_args...);
             }
         }
 
-        std::vector<std::function<T(Us...)>> m_vecSlots;
+    private:
+        std::vector<std::function<void(Us...)>> m_vecSlots;
     };
 
     /*****************************************************************
     * Here are some common example SignalType variants 
     ******************************************************************/
     template <typename T>
-    class SignalValueChanged : public SignalType<void, T>
+    class SignalValueChanged : public SignalType<T>
     {
     public:
         virtual ~SignalValueChanged() { /* NOP */ }
 
         void OnSignal_ValueChanged(const std::function<void(const T&)>& p_slotFunc) {
-            SignalType<void, T>::OnSignal(p_slotFunc);
+            SignalType<T>::OnSignal(p_slotFunc);
         }
 
+    protected:
         void EmitSignal_ValueChanged(const T& p_newValue) {
-            SignalType<void, T>::Emit(p_newValue);
+            SignalType<T>::Emit(p_newValue);
         }
     };
+
     template<typename... T>
-    class SignalFilterValue : SignalType<void, T...>
+    class SignalFilterValue : SignalType<T...>
     {
     public:
         virtual ~SignalFilterValue() { /*NOP*/ }
 
         void OnSignal_FilterValue(const std::function<void(T...)>& p_slotFunc) {
-            SignalType<void, T...>::OnSignal(p_slotFunc);
+            SignalType<T...>::OnSignal(p_slotFunc);
         }
 
+    protected:
         void EmitSignal_FilterValue(T... p_value) {
-            SignalType<void, T...>::Emit(p_value...);
+            SignalType<T...>::Emit(p_value...);
+        }
+    };
+
+    template<typename T>
+    class SignalAlertEvent : SignalType<T>
+    {
+    public:
+        virtual ~SignalAlertEvent() { /*NOP*/ }
+
+        void OnSignal_AlertEvent(T p_event, const std::function<void()>& p_slotFunc) {
+            SignalType<T>::template OnSpecificSignal<T>(p_event, p_slotFunc);
+        }
+
+    protected:
+        void EmitSignal_AlertEvent(T p_event) {
+            SignalType<T>::Emit(p_event);
         }
     };
 
